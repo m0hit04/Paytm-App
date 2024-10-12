289b6368b0d5e0084c257ad2925e445be8fe8c61
m0hit04 <msrathore0481@gmail.com>
2024-10-12 17:37:23 +0530

    Implemented SignOut in dashboard

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,4 +1,6 @@
+import {useNavigate} from "react-router-dom"
 export const Appbar = () => {
+    const navigate = useNavigate();
     return <>
         <div className="shadow h-14 flex justify-between">
             <div className="flex flex-col justify-center h-full ml-4">
@@ -13,8 +15,11 @@ export const Appbar = () => {
                         U
                     </div>
                 </div>
-                <div>
-                    <button>Sign out</button>
+                <div className="flex justify-center pt-2 h-25 w-28">
+                    <button type="button" className=" flex justify-center text-gray-900 border hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={() => {
+                        localStorage.removeItem("token");
+                        navigate("/")
+                    }}>Sign Out</button>
                 </div>
             </div>
         </div>
