96931b637cfc47b9f14b62b75e9c446b79f4ed7e
m0hit04 <msrathore0481@gmail.com>
2024-10-18 16:59:19 +0530

    Hook functionality inside useEffect

--- frontend/src/hooks/useFetchBalance.js
+++ frontend/src/hooks/useFetchBalance.js
@@ -1,25 +1,24 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useFetchBalance = () => {
     const [balance, setBalance] = useState(0);
-    try {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        return {success: false, balance};
-      }
-      axios.get("http://localhost:3000/api/v1/account/balance", {
-        headers: {
-          Authorization: `Bearer ${token}`
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            return { success: false, balance };
         }
-      }).then((response) => {
-          setBalance(response.data.balance);
-      })
-    } catch (err) {
-      console.log(err);
-      return {success: false, balance}
-    }
-    return {success: true, balance}
+        axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            setBalance(response.data.balance);
+        }).catch((err) => {
+            console.log(err);
+            return { success: false, balance }
+        })}, [balance])
+    return { success: true, balance }
 };
 
 export default useFetchBalance;
